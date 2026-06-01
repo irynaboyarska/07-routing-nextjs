86e4a7367043c4a95d9b77ef589a278f6f77ec5e
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-06-01 21:58:58 +0300

    Initial commit

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,43 @@
+import { createPortal } from "react-dom";
+import css from "./Modal.module.css";
+import { useEffect } from "react";
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export const Modal = ({ onClose, children }: ModalProps) => {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      onClick={handleBackdropClick}
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className={css.modal}>{children}</div>
+    </div>,
+    document.body,
+  );
+};
