7887757b53090c2187cef0bf83ecb6066bf6d08d
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-06-02 18:41:07 +0300

    fix 2

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import NotePreview from '@/components/NotePreview/NotePreview';
+import NotePreview from './NotePreview.client';
 import { Modal } from '@/components/Modal/Modal';
 
 interface NotePreviewModalProps {
